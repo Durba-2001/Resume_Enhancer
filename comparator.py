88a3182266a70c1668e04b5d0a038115712d31e4
Durba-2001 <durba028@gmail.com>
2025-08-07 12:13:27 +0530

    Delete comparator.py

--- comparator.py
+++
@@ -1,7 +0,0 @@
-from utils import print_separator
-
-def compare_version(original, enhanced):
-    print_separator("Original Resume")
-    print(original)
-    print_separator("Enhanced Resume")
-    print(enhanced)
